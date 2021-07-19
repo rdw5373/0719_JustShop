c6ed0af8e418749578384d8b742560af1f93bd8e
rdw5373 <rdw5373@naver.com>
2021-07-19 13:59:07 +0900

    0719_JustShop

---
+++ script/script.js
@@ -0,0 +1,33 @@
+// eslint-disable
+
+$(function(){
+
+    // GNB 메뉴 시작
+    $(".mainLi").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+    });
+
+    $(".mainLi").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+    });
+    // GNB 메뉴 끝
+
+
+    // 슬라이드쇼 시작
+    setInterval(fnSlide, 3000);
+
+    function fnSlide() {
+        
+        $("#shuttleFrame").animate(
+            {"margin-top": "-300px"}, 
+            1000, 
+            function(){
+                $("#slide a:first-child")
+                        .insertAfter("#slide a:last-child");
+                $("#shuttleFrame").css({"margin-top": "0"});
+            });
+
+    }
+    // 슬라이드쇼 끝
+
+});
